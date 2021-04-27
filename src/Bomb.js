a83cc6c421edcebd3396f590896dd163dcc5e2f4
Jeremy Wood <js.wood206@gmail.com>
2021-04-27 09:23:43 -0700

    commit tree 47b94571424d04dba3d8e1b1610b001e54a99ec9

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,27 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+    
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    timer() {
+        if (this.state.secondsLeft >= 1) {
+            return `${this.state.secondsLeft} seconds left before I go boom!`
+        } else {
+            return 'Boom!'
+        }
+    } 
+
+    render() {
+        return (
+            <div>
+                {this.timer()}
+            </div>
+        )
+    }
+}
